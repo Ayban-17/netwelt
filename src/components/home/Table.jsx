6c9ba143a87cf53548d9fa8970d6284ec4c6dafb
Ayban <mrkvnhrnndz17@gmail.com>
2023-08-11 03:16:23 +0800

    Recursion of Parent and children Done

--- src/components/home/Table.jsx
+++ src/components/home/Table.jsx
@@ -1,6 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const buildHierarchy = (data) => {
+
+  const territories = {};
+
+  data.forEach(item => {
+    const { id, name, parent } = item;
+    const territory = { id, name, children: [] };
+
+    if (parent === null) {
+      territories[id] = territory;
+    } else {
+      const parentTerritory = territories[parent];
+      if (parentTerritory) {
+        parentTerritory.children.push(territory);
+      }
+    }
+  });
+
+  return Object.values(territories);
+}
+
+
 const Table = () => {
 
   const [places, setPlaces] = useState([]);
@@ -11,25 +33,32 @@ const Table = () => {
      
       try {
         const response = await axios.get(url);
-        setPlaces(response.data.data);
+        const hierarchicalData = buildHierarchy(response.data.data);
+        setPlaces(hierarchicalData);
       } catch (error) {
         console.log(error.data);
       }
     };
     fetchPlaces();
+   
   }, []);
+
+  // Recursive function to render territories
+  const renderTerritory = (territory, indent = 0) => (
+    <li key={territory.id} className={`border-2 w-56 mx-0 my-auto ${indent > 0 ? 'ml-4' : ''}`}>
+      {territory.name}
+      {territory.children.length > 0 && (
+        <ul className="mt-2">
+          {territory.children.map(child => renderTerritory(child, indent + 1))}
+        </ul>
+      )}
+    </li>
+  );
 
   return (
     <div>
-      <ul>
-        {places.map(({name, id, parent})=>(
-          <li className="border-2 w-56 mx-0 my-auto" key={id}>
-          name:  {name}, 
-          <br /> 
-          parentID: {parent}, 
-          <br /> 
-          ID: {id}</li>
-        ))}
+      <ul className="flex flex-wrap gap-4">
+        {places.map(place => renderTerritory(place))}
       </ul>
     </div>
   );
